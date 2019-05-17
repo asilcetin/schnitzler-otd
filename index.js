d664a29e91406c66ddc5a8aecfa4c51f6b72a9f5
Asil Çetin <25176039+asilcetin@users.noreply.github.com>
2019-05-17 13:48:16 +0200

    added astb api request

--- index.js
+++ index.js
@@ -2,47 +2,43 @@ const config = require('./config');
 const axios = require('axios');
 const Twit = require('twit');
 
+const todaysDate = new Date();
+const dd = String(todaysDate.getDate()).padStart(2, '0');
+const mm = String(todaysDate.getMonth() + 1).padStart(2, '0');
+const yyyy = todaysDate.getFullYear();
+const yyyyJournal = yyyy - 123;
 
-axios.get(config.astb.api + '/collections/editions/entry__1900-01-01.xml?format=text')
+axios.get(config.astb.api + '/collections/editions/entry__'+yyyyJournal+'-'+mm+'-'+dd+'.xml?format=text')
   .then(response => {
-    console.log(response.data.trim());
+    postTweet(response.data.trim());
   })
   .catch(error => {
     console.log(error);
   });
 
-var T = new Twit({
-  consumer_key: config.twitter.consumer_key,
-  consumer_secret: config.twitter.consumer_secret,
-  access_token: config.twitter.access_token,
-  access_token_secret: config.twitter.access_token_secret,
-})
+function postTweet(payload) {
+  const T = new Twit({
+    consumer_key: config.twitter.consumer_key,
+    consumer_secret: config.twitter.consumer_secret,
+    access_token: config.twitter.access_token,
+    access_token_secret: config.twitter.access_token_secret,
+  })
 
+  let diaryContent = payload;
+  if (diaryContent.length > 190) diaryContent = diaryContent.slice(0, 190) + '[...]';
+  const diaryEntryLink = config.astb.gui + yyyyJournal+'-'+mm+'-'+dd+'.xml';
+  const tweetContent = '#OTD vor 123 Jahren im Tagebuch von #ArthurSchnitzler:\n\n' + diaryContent + '\n\nWeiter: ' + diaryEntryLink;
 
-T.get('account/verify_credentials', {
-  include_entities: false,
-  skip_status: true,
-  include_email: false
-}, onAuthenticated)
+  T.post('statuses/update', {
+    status: tweetContent
+  }, onTweeted)
 
-
-function onAuthenticated(err, res) {
-  if (err) {
-      throw err
-  }
-  console.log('Authentication successful. Running bot...\r\n')
 }
-
-
-T.post('statuses/update', {
-  status: "Tweeting my first automated tweet."
-}, onTweeted)
-
 
 function onTweeted(err, reply) {
   if (err !== undefined) {
-      console.log(err)
+    console.log(err)
   } else {
-      console.log('Tweeted: ' + reply.text)
+    console.log('Tweeted: ' + reply.text)
   }
 }
